7b8cad0f3872d9d4be79bb7ac962ddcc451b131c
Obansa John <65238842+ozo-vehe@users.noreply.github.com>
2021-04-13 16:55:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 let hamMenu = document.querySelector('.nav-bar');
 let navLinks  = document.querySelector('.nav-links');
+let eyeIcon = document.querySelector("#visible-img");
+let eyeIconHidden = document.querySelector("#invisible-img");
+
 
 /**********Add Click Event To The Hamburger**********/
 hamMenu.addEventListener('click', function() {
     navLinks.classList.toggle('dropdown');
 })
+
+eyeIcon.addEventListener("click", function() {
+    eyeIconHidden.classList.remove("hidden");
+    eyeIcon.classList.add("hidden");
+    password.setAttribute("type", "text");
+});
+
+eyeIconHidden.addEventListener("click", function() {
+    eyeIconHidden.classList.add("hidden");
+    eyeIcon.classList.remove("hidden");
+    password.setAttribute("type", "password")
+});
